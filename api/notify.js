a0c11b751892f6a76dfbe48a994b3df2b48782da
DJPADESCO <212222493+DJPADESCO@users.noreply.github.com>
2026-05-25 02:36:17 +0000

    Update notify.js

--- api/notify.js
+++ api/notify.js
@@ -7,7 +7,7 @@ function getApp() {
   if (getApps().length) return getApps()[0];
   return initializeApp({
     credential: cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
+      projectId: process.env.FIREBASE_PROJECT_ID_ONE,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n')
     })
