9dfa3c8605c3ff7dc137d84a9cd4e2e643d22006
DJPADESCO <212222493+DJPADESCO@users.noreply.github.com>
2026-05-25 02:59:06 +0000

    Update notify.js

--- api/notify.js
+++ api/notify.js
@@ -7,10 +7,10 @@ function getApp() {
   if (getApps().length) return getApps()[0];
   return initializeApp({
     credential: cert({
-      projectId: process.env.FIREBASE_PROJECT_ID_ONE,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n')
-    })
+  projectId: process.env.FIREBASE_PROJECT_ID_ONE || process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL_ONE || process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: (process.env.FIREBASE_PRIVATE_KEY_ONE || process.env.FIREBASE_PRIVATE_KEY)?.replace(/\\n/g, '\n')
+})
   });
 }
 
