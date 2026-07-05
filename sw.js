ed43db75ef8480e36fb3c1db92ed3ef592f1f04a
DJPADESCO <212222493+DJPADESCO@users.noreply.github.com>
2026-07-05 04:00:39 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.07.04.1'; // Modifiez cette date à chaque mise à jour de votre site
+const APP_VERSION = '2026.07.05.1'; // Modifiez cette date à chaque mise à jour de votre site
 const CACHE_NAME = `joband-cache-v-${APP_VERSION}`; // Crée le nom du cache automatiquement
 
 // Fichiers vitaux mis en cache au démarrage
