335fc89c98e77ae64ff5671aa41c63ceb7cc279c
DJPADESCO <212222493+DJPADESCO@users.noreply.github.com>
2026-07-07 07:03:42 +0000

    Update script.js

--- script.js
+++ script.js
@@ -785,6 +785,7 @@ document.addEventListener('click', e => {
     initTTS();
     initPWA();
     initTestimonials();
+   initEventsCalendar()
 /* ── NOTIFICATIONS PUSH ── */
 function initNotifications() {
     const VAPID_KEY = 'BEmflxm0W984vE7ZPJ9ADXn2ZfJhUyFVn1pY7lq9d02L1rpgjofPZrcBdaV-s6gARn1_MdnpTM1ZJCrZKWE3p1E';
