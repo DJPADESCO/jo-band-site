fb5930027ab1f4295e8ace5537219858ffef60ad
DJPADESCO <212222493+DJPADESCO@users.noreply.github.com>
2026-07-09 08:40:58 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.07.09.2'; // Changez ce numéro pour forcer la mise à jour
+const APP_VERSION = '2026.07.09.3'; // Changez ce numéro pour forcer la mise à jour
 const CACHE_NAME = `joband-cache-v-${APP_VERSION}`;
 
 const ASSETS = [
