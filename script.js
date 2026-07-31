bfc4b22627f5c9b16e278d90b88d502f9ec8e2c4
DJPADESCO <212222493+DJPADESCO@users.noreply.github.com>
2026-07-31 10:17:50 +0000

    Update script.js

--- script.js
+++ script.js
@@ -715,6 +715,7 @@ function initEventCountdown() {
     startCounters();
     setDailyQuote(currentLang);
     buildMembersGrid();
+    loadContactContent();
     initGalleryFilters();
 
     // Modale membres
